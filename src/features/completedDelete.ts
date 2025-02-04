4197daa39e76b413142dddc192dd069b11841156
hayeon <qdt0650@naver.com>
2025-02-04 14:41:02 +0900

    refactor: 완료된 할 일 삭제기능 타입스크립트로 변환

--- src/features/completedDelete.ts
+++ src/features/completedDelete.ts
@@ -1,8 +1,10 @@
 // 완료된 할 일 삭제
 
-export const completedDelete = (saveToDos, todos) => {
+import { TodoItem } from './add'
+
+export const completedDelete = (saveToDos: any, todos: TodoItem[]) => {
    // 완료삭제 버튼 요소를 가져옵니다.
-   const completeTodoDeleteButton = document.querySelector('#completeTodoDeleteButton')
+   const completeTodoDeleteButton = document.querySelector('#completeTodoDeleteButton') as HTMLButtonElement
 
    completeTodoDeleteButton.addEventListener('click', () => {
       // 완료처리된 요소들을 모두 가져옵니다.
@@ -11,7 +13,9 @@ export const completedDelete = (saveToDos, todos) => {
       completeTodo.forEach((todo) => {
          // 완료처리된 요소들의 부모를 찾아 전체삭제 해줍니다.
          const completeTodoContent = todo.closest('.todo_content')
-         completeTodoContent.remove()
+
+         // completeTodoContent가 null 일 수도 있다. 옵셔널체이닝 사용해서 null 타입 체크크
+         completeTodoContent?.remove()
 
          // 로컬스토리지 데이터에서 완료처리된(true) 항목들을 제거하고 다시 저장해준다.
          todos = todos.filter((x) => x.completeButton !== true)
