c10810bf64e2d129883246a9aa0c19d71d089271
hayeon <qdt0650@naver.com>
2025-02-04 14:09:50 +0900

    refactor: 할 일 선택기능 타입스크립트로 변환

--- src/utilities/checkboxEvent.ts
+++ src/utilities/checkboxEvent.ts
@@ -1,5 +1,7 @@
+import { TodoItem } from '../features/add'
+
 // 체크박스 선택 이벤트
-export const checkboxEvent = (todos, saveToDos) => {
+export const checkboxEvent = (todos: TodoItem[], saveToDos: any) => {
    const checkboxAll = document.querySelectorAll('.todo_checkboxes')
 
    // 모든 checkbox들에 change이벤트를 준다.
@@ -7,7 +9,7 @@ export const checkboxEvent = (todos, saveToDos) => {
       checkbox.addEventListener('change', (event) => {
          checkbox.setAttribute('checked', '')
          // todo배열 index번째 todoCheck 속성을 변경된 checked값으로 바꿔준다.
-         todos[index].todoCheck = event.target.checked
+         todos[index].todoCheck = (event.target as HTMLInputElement).checked
          // 로컬스토리지에 변경된 데이터를 저장한다.
          saveToDos(todos)
       })
